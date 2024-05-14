11408953bc135185086bc1343ab61debf5732d06
xiayu_zhou <3151293303@qq.com>
2024-05-14 21:50:45 +0800

    update

--- linux_drv/v4l2_drv/Test/test.c
+++ linux_drv/v4l2_drv/Test/test.c
@@ -9,6 +9,7 @@
 #include <fcntl.h>
 #include <sys/ioctl.h>
 #include <linux/videodev2.h>
+#include <string.h>
 
 #define PATH_NAME "/dev/video0"
 
@@ -23,11 +24,81 @@ int main(void)
     }
     printf("fd = %d\n",fd);
 
-    if (ioctl(fd, VIDIOC_QUERYCAP, cap) < 0)
+    int ret;
+    if ((ret = ioctl(fd, VIDIOC_QUERYCAP, &cap)) < 0)
     {
-        printf("ERR(%s):VIDIOC_QUERYCAP failed\n", __func__);
+        printf("ERR(%s):VIDIOC_QUERYCAP failed,ret = %d\n", __func__,ret);
         return -1;
     }
+
+    if(cap.capabilities & V4L2_CAP_VIDEO_CAPTURE)
+        printf("v4l2 dev support capture\n");
+
+    if(cap.capabilities & V4L2_CAP_VIDEO_OUTPUT)
+        printf("v4l2 dev support output\n");
+
+    if(cap.capabilities & V4L2_CAP_VIDEO_OVERLAY)
+        printf("v4l2 dev support overlay\n");
+
+    if(cap.capabilities & V4L2_CAP_STREAMING)
+        printf("v4l2 dev support streaming\n");
+
+    if(cap.capabilities & V4L2_CAP_READWRITE)
+        printf("v4l2 dev support read write\n");
+
+
+
+/*
+    struct v4l2_input input;
+    input.index = 0;
+    while (!ioctl(fd, VIDIOC_ENUMINPUT, &input))
+    {
+        printf("index = %d,input:%s\n",input.index,input.name);
+        ++input.index;
+    }
+*/
+
+    struct v4l2_input input;
+
+    input.index = 0; //指定输入设备
+
+    if (ioctl(fd, VIDIOC_S_INPUT, &input) < 0)
+    {
+        printf("ERR(%s):VIDIOC_S_INPUT failed\n", __func__);
+        return -1;
+    }
+
+
+    struct v4l2_fmtdesc fmtdesc;
+    fmtdesc.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    fmtdesc.index = 0;
+
+    while (!ioctl(fd, VIDIOC_ENUM_FMT, &fmtdesc))
+    {
+        printf("fmt:%s\n", fmtdesc.description);
+
+        fmtdesc.index++;
+    }
+
+
+    struct v4l2_format v4l2_fmt;
+
+    int width = 1920;
+    int height = 1080;
+
+    memset(&v4l2_fmt, 0, sizeof(struct v4l2_format));
+    v4l2_fmt.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    v4l2_fmt.fmt.pix.width = width; //宽度
+    v4l2_fmt.fmt.pix.height = height; //高度
+    v4l2_fmt.fmt.pix.pixelformat = V4L2_PIX_FMT_YUYV; //像素格式
+    v4l2_fmt.fmt.pix.field = V4L2_FIELD_ANY;
+
+    if (ioctl(fd, VIDIOC_S_FMT, &v4l2_fmt) < 0)
+    {
+        printf("ERR(%s):VIDIOC_S_FMT failed\n", __func__);
+        return -1;
+    }
+
 
     close(fd);
     return 0;
