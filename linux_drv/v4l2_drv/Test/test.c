b036a0631964d2f10e139ba42fb9ebe8ec75b205
xiayu_zhou <3151293303@qq.com>
2024-05-20 09:21:28 +0800

    update

--- linux_drv/v4l2_drv/Test/test.c
+++ linux_drv/v4l2_drv/Test/test.c
@@ -10,6 +10,8 @@
 #include <sys/ioctl.h>
 #include <linux/videodev2.h>
 #include <string.h>
+#include <sys/mman.h>
+#include <sys/poll.h>
 
 #define PATH_NAME "/dev/video0"
 
@@ -47,17 +49,6 @@ int main(void)
         printf("v4l2 dev support read write\n");
 
 
-
-/*
-    struct v4l2_input input;
-    input.index = 0;
-    while (!ioctl(fd, VIDIOC_ENUMINPUT, &input))
-    {
-        printf("index = %d,input:%s\n",input.index,input.name);
-        ++input.index;
-    }
-*/
-
     struct v4l2_input input;
 
     input.index = 0; //指定输入设备
@@ -83,8 +74,8 @@ int main(void)
 
     struct v4l2_format v4l2_fmt;
 
-    int width = 1920;
-    int height = 1080;
+    int width = 1280;
+    int height = 720;
 
     memset(&v4l2_fmt, 0, sizeof(struct v4l2_format));
     v4l2_fmt.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
@@ -99,6 +90,91 @@ int main(void)
         return -1;
     }
 
+    struct v4l2_requestbuffers req;
+    req.count = 30; //缓存数量
+    req.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    req.memory = V4L2_MEMORY_MMAP;
+    if (ioctl(fd, VIDIOC_REQBUFS, &req) < 0)
+    {
+        printf("ERR(%s):VIDIOC_REQBUFS failed\n", __func__);
+        return -1;
+    }
+
+
+    struct v4l2_buffer v4l2_buffer;
+    for(int i = 0; i < 30; i++)
+    {
+        memset(&v4l2_buffer, 0, sizeof(struct v4l2_buffer));
+        v4l2_buffer.index = i; //想要放入队列的缓存
+        v4l2_buffer.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+        v4l2_buffer.memory = V4L2_MEMORY_MMAP;
+
+        ret = ioctl(fd, VIDIOC_QBUF, &v4l2_buffer);
+        if(ret < 0)
+        {
+            printf("Unable to queue buffer.\n");
+            return -1;
+        }
+    }
+
+
+    //打开设备
+    enum v4l2_buf_type type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+
+    if (ioctl(fd, VIDIOC_STREAMON, &type) < 0)
+    {
+        printf("ERR(%s):VIDIOC_STREAMON failed\n", __func__);
+        return -1;
+    }
+
+
+
+    //等待
+    struct pollfd poll_fds[1];
+    poll_fds[0].fd = fd;
+    poll_fds[0].events = POLLIN; //等待可读
+
+    printf("waiting read\n");
+
+    //出队
+    poll(poll_fds, 30, 10000);
+    struct v4l2_buffer buffer;
+    buffer.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    buffer.memory = V4L2_MEMORY_MMAP;
+    if (ioctl(fd, VIDIOC_DQBUF, &buffer) < 0)
+    {
+        printf("ERR(%s):VIDIOC_DQBUF failed, dropped frame\n", __func__);
+        return -1;
+    }
+
+    printf("出队完成\n");
+
+/*    //入队
+    struct v4l2_buffer v4l2_buf;
+
+    v4l2_buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    v4l2_buf.memory = V4L2_MEMORY_MMAP;
+    v4l2_buf.index = 0; //指定buf
+
+    if (ioctl(fd, VIDIOC_QBUF, &v4l2_buf) < 0)
+    {
+        printf("ERR(%s):VIDIOC_QBUF failed\n", __func__);
+        return -1;
+    }
+
+
+
+
+    //释放资源
+    if (ioctl(fd, VIDIOC_STREAMOFF, &type) < 0)
+    {
+        printf("ERR(%s):VIDIOC_STREAMOFF failed\n", __func__);
+        return -1;
+    }*/
+
+
+/*    for(int i = 0; i < 30; ++i)
+        munmap(buf[i].addr, buf[i]->length);*/
 
     close(fd);
     return 0;
